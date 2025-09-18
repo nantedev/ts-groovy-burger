f885bdb68f8a6426d32a2dc20b344e80ec4f7489
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-09-18 13:48:36 +0400

    TSE-18: import burger background

--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,6 +1,6 @@
-import styled from "styled-components"
-import Logo from "@/components/reusable-ui/Logo"
-import LoginForm from "./LoginForm"
+import styled from "styled-components";
+import Logo from "@/components/reusable-ui/Logo";
+import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
   return (
@@ -8,7 +8,7 @@ export default function LoginPage() {
       <Logo className={"logo-login-page"} />
       <LoginForm />
     </LoginPageStyled>
-  )
+  );
 }
 
 const LoginPageStyled = styled.div`
@@ -20,7 +20,7 @@ const LoginPageStyled = styled.div`
 
   ::before {
     content: "";
-    background: url("/images/burger-and-fries-background.jpg") rgba(0, 0, 0, 0.7);
+    background: url("/images/burger-right.jpg") rgba(0, 0, 0, 0.3);
     background-size: cover;
     background-position: center;
     background-blend-mode: darken;
@@ -36,4 +36,4 @@ const LoginPageStyled = styled.div`
   .logo-login-page {
     transform: scale(2.5);
   }
-`
+`;
