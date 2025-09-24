d8fc98829369e34d2ab41871cb3de0e071a93180
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-09-24 23:24:11 +0400

    TSE-22 : Modal Shortcuts > Prévenir event propagation sur le bouton

--- src/components/pages/order/Main/MainLeftSide/Admin/ModalShortcurts.tsx
+++ src/components/pages/order/Main/MainLeftSide/Admin/ModalShortcurts.tsx
@@ -9,12 +9,18 @@ type ModalShortcutsProps = {
 export const ModalShortcurts = ({ onClick }: ModalShortcutsProps) => {
   const raccourci = isMac() ? "⌘" : "Ctrl";
 
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    // Vérifie si onClick existe avant de l'appeler (évite les erreurs si undefined)
+    onClick && onClick(event);
+  };
+
   return (
     <ModalShortcurtsStyled>
       <span className="title">💡 Pour aller plus vite :</span>
       <span className="shortcut-message i">{`${raccourci} + i : Toggle "mode" admin`}</span>
       <span className="shortcut-message j">{`${raccourci} + j : Toggle "panel" admin`}</span>
-      <button onClick={onClick} className="shortcut-message">
+      <button onClick={handleClick} className="shortcut-message">
         Ne plus rappeler
       </button>
     </ModalShortcurtsStyled>
@@ -42,5 +48,21 @@ const ModalShortcurtsStyled = styled.div`
 
   .shortcut-message {
     font-size: ${theme.fonts.size.SM};
+  }
+
+  button {
+    padding: 13px 58px;
+    border-radius: 25px;
+    cursor: pointer;
+    color: ${theme.colors.white};
+    background-color: transparent;
+    border: 1px solid ${theme.colors.white};
+    :hover {
+      color: ${theme.colors.primary};
+      border: 1px solid ${theme.colors.primary};
+    }
+    :active {
+      opacity: 50%;
+    }
   }
 `;
